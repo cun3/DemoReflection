5e7501582400052b6876728efb28ca63eb6156a0
cun3 <nachiketh1989@gmail.com>
2016-02-29 13:05:50 -0500

    Initial Commit

---
+++ ReflectionDemo/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ReflectionDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDetails = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblMethods = new System.Windows.Forms.Label();
+            this.lblProperties = new System.Windows.Forms.Label();
+            this.lblConstructors = new System.Windows.Forms.Label();
+            this.txtTypeName = new System.Windows.Forms.TextBox();
+            this.btnDiscoverType = new System.Windows.Forms.Button();
+            this.lstMethods = new System.Windows.Forms.ListBox();
+            this.lstProperties = new System.Windows.Forms.ListBox();
+            this.lstConstructors = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblDetails
+            // 
+            this.lblDetails.AutoSize = true;
+            this.lblDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDetails.Location = new System.Drawing.Point(462, 33);
+            this.lblDetails.Name = "lblDetails";
+            this.lblDetails.Size = new System.Drawing.Size(144, 25);
+            this.lblDetails.TabIndex = 0;
+            this.lblDetails.Text = "Type Details";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(82, 109);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(98, 20);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Type Name";
+            // 
+            // lblMethods
+            // 
+            this.lblMethods.AutoSize = true;
+            this.lblMethods.Location = new System.Drawing.Point(85, 171);
+            this.lblMethods.Name = "lblMethods";
+            this.lblMethods.Size = new System.Drawing.Size(48, 13);
+            this.lblMethods.TabIndex = 2;
+            this.lblMethods.Text = "Methods";
+            // 
+            // lblProperties
+            // 
+            this.lblProperties.AutoSize = true;
+            this.lblProperties.Location = new System.Drawing.Point(396, 171);
+            this.lblProperties.Name = "lblProperties";
+            this.lblProperties.Size = new System.Drawing.Size(54, 13);
+            this.lblProperties.TabIndex = 3;
+            this.lblProperties.Text = "Properties";
+            // 
+            // lblConstructors
+            // 
+            this.lblConstructors.AutoSize = true;
+            this.lblConstructors.Location = new System.Drawing.Point(716, 171);
+            this.lblConstructors.Name = "lblConstructors";
+            this.lblConstructors.Size = new System.Drawing.Size(66, 13);
+            this.lblConstructors.TabIndex = 4;
+            this.lblConstructors.Text = "Constructors";
+            // 
+            // txtTypeName
+            // 
+            this.txtTypeName.Location = new System.Drawing.Point(220, 109);
+            this.txtTypeName.Name = "txtTypeName";
+            this.txtTypeName.Size = new System.Drawing.Size(298, 20);
+            this.txtTypeName.TabIndex = 5;
+            // 
+            // btnDiscoverType
+            // 
+            this.btnDiscoverType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDiscoverType.Location = new System.Drawing.Point(622, 101);
+            this.btnDiscoverType.Name = "btnDiscoverType";
+            this.btnDiscoverType.Size = new System.Drawing.Size(150, 23);
+            this.btnDiscoverType.TabIndex = 6;
+            this.btnDiscoverType.Text = "Discover Type info.";
+            this.btnDiscoverType.UseVisualStyleBackColor = true;
+            this.btnDiscoverType.Click += new System.EventHandler(this.btnDiscoverType_Click);
+            // 
+            // lstMethods
+            // 
+            this.lstMethods.FormattingEnabled = true;
+            this.lstMethods.Location = new System.Drawing.Point(88, 206);
+            this.lstMethods.Name = "lstMethods";
+            this.lstMethods.Size = new System.Drawing.Size(209, 329);
+            this.lstMethods.TabIndex = 7;
+            // 
+            // lstProperties
+            // 
+            this.lstProperties.FormattingEnabled = true;
+            this.lstProperties.Location = new System.Drawing.Point(399, 206);
+            this.lstProperties.Name = "lstProperties";
+            this.lstProperties.Size = new System.Drawing.Size(217, 329);
+            this.lstProperties.TabIndex = 8;
+            this.lstProperties.SelectedIndexChanged += new System.EventHandler(this.lstProperties_SelectedIndexChanged);
+            // 
+            // lstConstructors
+            // 
+            this.lstConstructors.FormattingEnabled = true;
+            this.lstConstructors.Location = new System.Drawing.Point(719, 206);
+            this.lstConstructors.Name = "lstConstructors";
+            this.lstConstructors.Size = new System.Drawing.Size(213, 329);
+            this.lstConstructors.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(988, 629);
+            this.Controls.Add(this.lstConstructors);
+            this.Controls.Add(this.lstProperties);
+            this.Controls.Add(this.lstMethods);
+            this.Controls.Add(this.btnDiscoverType);
+            this.Controls.Add(this.txtTypeName);
+            this.Controls.Add(this.lblConstructors);
+            this.Controls.Add(this.lblProperties);
+            this.Controls.Add(this.lblMethods);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblDetails);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnDiscover;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblDetails;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblMethods;
+        private System.Windows.Forms.Label lblProperties;
+        private System.Windows.Forms.Label lblConstructors;
+        private System.Windows.Forms.TextBox txtTypeName;
+        private System.Windows.Forms.Button btnDiscoverType;
+        private System.Windows.Forms.ListBox lstMethods;
+        private System.Windows.Forms.ListBox lstProperties;
+        private System.Windows.Forms.ListBox lstConstructors;
+    }
+}
+
---
+++ ReflectionDemo/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace ReflectionDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void lstProperties_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+            
+        private void btnDiscoverType_Click(object sender, EventArgs e)
+        {
+            string typeName = txtTypeName.Text;
+            Type T = Type.GetType(typeName);
+
+            lstMethods.Items.Clear();
+            lstProperties.Items.Clear();
+            lstConstructors.Items.Clear();
+
+            MethodInfo[] methods = T.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                lstMethods.Items.Add(method.ReturnType.Name + " "+ method.Name);
+            }
+
+
+            PropertyInfo[] properties = T.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                lstProperties.Items.Add(property.PropertyType.Name + " " + property.Name);
+            }
+
+
+            ConstructorInfo[] constructors = T.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                lstConstructors.Items.Add(constructor.ToString());
+            }
+
+        }
+
+        
+
+       
+    }
+}
